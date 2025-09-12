bb6682fc9d9f0428cfe1ce34328559281d871f49
Oba089 <nadematiq89@gmail.com>
2025-09-12 16:25:31 -0700

    first commit

---
+++ java/Squares.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+	public class Squares {
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("Enter a number");
+	double number = input.nextDouble();
+
+ 	double square = Math.pow(number, 2);
+
+	System.out.println(square);
+}
+}
+
+
+	
+
+	
+	
