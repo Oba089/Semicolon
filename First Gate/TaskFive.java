1627caf1a4f65821055e56e69143b0b04b552034
Oba089 <nadematiq89@gmail.com>
2025-09-26 11:54:51 -0700

    First Gate

---
+++ First Gate/TaskFive.java
@@ -0,0 +1,14 @@
+public class TaskFive {
+	public static void main(String[] args) {
+
+	for(int a = 4 ; a <= 10; a = a +4) {
+
+	for(int b = 1; b < a; a = a+1) {
+
+	if(1 >)
+
+	System.out.print(a);
+}
+}
+}
+}
---
+++ First Gate/TaskThree.java
@@ -0,0 +1,9 @@
+public class TaskThree {
+	public static void main(String[] args) {
+	for(int count = 3; count <= 10; count = count +2) {
+
+	System.out.print(count);
+ 
+}
+}
+}
