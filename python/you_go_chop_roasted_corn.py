5155d8fd6f33ffd3b3e82e39b385e1f39fcaaafa
Oba089 <nadematiq89@gmail.com>
2025-10-13 11:36:11 -0700

    FiFtH CommiTs

--- python/you_go_chop_roasted_corn.py
+++ python/you_go_chop_roasted_corn.py
@@ -1,12 +1,4 @@
-def first_last_2_chars(s):
 
-    	if len(s) < 2:
-        return ''
-
-    	return s[:2] + s[-2:]
-
-
-print(first_last_2_chars('!hello!'))  
 
 
 def add_ing_or_ly(s):
@@ -37,21 +29,6 @@ def longest_word(words):
 
 print(f"The longest word is '{word}' with length {length}.")
 
-
-def remove_odd_index_chars(s):
-
-    return s[::2]
-
-
-print(remove_odd_index_chars("python"))
-
-
-def remove_odd_index_chars(s):
-
-    return s[::2]
-
-
-print(remove_odd_index_chars("python"))
 
 
 def find_min_number(numbers):
